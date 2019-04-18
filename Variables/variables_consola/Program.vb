574889560b165e51b936fa86799858f714e9b182
matias2112 <33470569+matias2112@users.noreply.github.com>
2019-04-18 15:35:12 -0300

    Nuevo

---
+++ Variables/variables_consola/Program.vb
@@ -0,0 +1,22 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Dim Nombre As String
+        Dim Apellido As String
+        Dim Fnacimiento
+
+        Console.Write("Ingrese Nombre:  ")
+        Nombre = Console.ReadLine()
+
+        Console.Write("Ingrese Apellido:   ")
+        Apellido = Console.ReadLine()
+
+        Console.Write("Ingrese Fecha de nacimiento:   ")
+        Fnacimiento = Console.ReadLine()
+
+        Console.WriteLine("Apellido: " & Apellido & "  " & "Nombre: " & Nombre & "  " & "Nacio el: " & Fnacimiento)
+
+    End Sub
+End Module
