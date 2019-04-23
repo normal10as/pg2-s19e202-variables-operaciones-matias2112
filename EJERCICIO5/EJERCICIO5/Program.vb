4ed45fef169dd623bf815f08a3a3c9b5bd0f69bf
user <user@LAPTOP-FHT64KL6>
2019-04-22 22:06:43 -0300

    nuevo

---
+++ EJERCICIO5/EJERCICIO5/Program.vb
@@ -0,0 +1,20 @@
+Imports System
+
+Module Ejercicio5
+    Sub Main()
+
+        Dim a As Integer
+        Dim b As Integer
+        Dim c As Integer
+        Dim x As Decimal
+
+        a = 1
+        b = 5
+        c = 2
+
+        x = ((b ^ 2 - 4 * a * c) / 2 * a)
+
+        Console.WriteLine("x =" & " " & x)
+
+    End Sub
+End Module
