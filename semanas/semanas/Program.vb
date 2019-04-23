7e0a4a5711c1629aa0ad9547f733650ed634298a
user <user@LAPTOP-FHT64KL6>
2019-04-22 22:10:42 -0300

    nuevo

---
+++ semanas/semanas/Program.vb
@@ -0,0 +1,29 @@
+Imports System
+
+Module Semanas
+    Sub Main()
+        Dim Nmuero1 As Integer
+        Dim Nmuero2 As Integer
+        Dim Nmuero3 As Integer
+        Dim Nmuero4 As Integer
+        Dim Suma As Integer
+
+        Console.Write("Ingrese un numero: ")
+        Nmuero1 = Console.ReadLine()
+
+        Console.Write("Ingrese un numero: ")
+        Nmuero2 = Console.ReadLine()
+
+        Console.Write("Ingrese un numero: ")
+        Nmuero3 = Console.ReadLine()
+
+        Console.Write("Ingrese un numero: ")
+        Nmuero4 = Console.ReadLine()
+
+
+        Suma = Nmuero1 + Nmuero2 + Nmuero3 + Nmuero4
+        Console.Write("La suma de los numeros ingresados es: " & Suma)
+
+
+    End Sub
+End Module
