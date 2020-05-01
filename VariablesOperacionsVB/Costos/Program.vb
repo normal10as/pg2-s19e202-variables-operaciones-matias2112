33003045b9c1e6afefdb6f2af0de6339ba1d6ba2
Santiago Del Rosario <delro_chinchu@live.com.ar>
2020-05-01 19:35:17 -0300

    Costo1.1

---
+++ VariablesOperacionsVB/Costos/Program.vb
@@ -0,0 +1,20 @@
+Imports System
+
+Module Costos
+    Sub Main(args As String())
+        Dim DescArticulo As String
+        Dim UnidadesProducidas As Byte
+        Dim Costo As Single
+
+        Console.Write("Ingrese la descripcion: ")
+        DescArticulo = Console.ReadLine
+        Console.Write("Ingrese las Unidades producidas: ")
+        UnidadesProducidas = Console.ReadLine
+        Costo = (UnidadesProducidas * 3.5) + 10700
+
+        Console.WriteLine("Descripciones: " & DescArticulo)
+        Console.WriteLine("Unidades Producidas: " & UnidadesProducidas)
+        Console.WriteLine("Costo: " & Costo)
+
+    End Sub
+End Module
