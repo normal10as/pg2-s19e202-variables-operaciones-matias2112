4c7846c61226cead3804b21d47956507a05d93ce
Santiago Del Rosario <delro_chinchu@live.com.ar>
2020-05-02 12:04:16 -0300

    1.7

---
+++ VariablesOperacionesCS/EquivalenciasPies/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EquivalenciasPies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Pies, Yarda, Pulgada, Centimetro, Metro;
+            Console.Write("Ingrese una medida en pies: ");
+            Pies = double.Parse(Console.ReadLine());
+            Pulgada = Pies * 12;
+            Yarda = Pies / 3;
+            Centimetro = Pulgada * 2.54;
+            Metro = Centimetro / 100;
+            Console.WriteLine(Yarda + " yardas");
+            Console.WriteLine(Pulgada + " pulgadas");
+            Console.WriteLine(Centimetro + " cm");
+            Console.WriteLine(Metro + " m");
+        }
+    }
+}
