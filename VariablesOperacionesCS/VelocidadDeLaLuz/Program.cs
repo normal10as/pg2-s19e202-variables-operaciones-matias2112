46e8d9ad864bbcbc525046dab37c4997c96ec102
Santiago Del Rosario <delro_chinchu@live.com.ar>
2020-05-02 11:56:52 -0300

    1.4

---
+++ VariablesOperacionesCS/VelocidadDeLaLuz/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VelocidadDeLaLuz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Segundos;
+        int Kilometros  = 300000;
+            Console.Write("Ingrese los segundos: ");
+            Segundos = double.Parse(Console.ReadLine());
+            Console.WriteLine("La distancia que recorre es de: " + Kilometros * Segundos + "km");
+        }
+    }
+}
