d3055d7c5d5ad28b0e2b3b3168b3753cc6adbc29
Santiago Del Rosario <delro_chinchu@live.com.ar>
2020-05-02 12:01:54 -0300

    1.6

---
+++ VariablesOperacionesCS/GradosFahrenheit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GradosFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Celsius, Fahrenheit;
+            Console.Write("Ingrese los grados Celsius: ");
+            Celsius = float.Parse(Console.ReadLine());
+            Fahrenheit = (Celsius * (9 / 5)) + 32;
+            Console.WriteLine(Fahrenheit + "°F");
+        }
+    }
+}
