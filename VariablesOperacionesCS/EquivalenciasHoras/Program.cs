8829a6196ee261ce9b6915999c36151b320e751b
Santiago Del Rosario <delro_chinchu@live.com.ar>
2020-05-02 11:50:54 -0300

    1.3

---
+++ VariablesOperacionesCS/EquivalenciasHoras/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EquivalenciasHoras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Hora, Minutos, Segundos, Dias ;
+            Console.Write("Ingrese las horas: ");
+            Hora = double.Parse(Console.ReadLine());
+            Minutos = Hora * 60;
+            Segundos = Minutos * 60;
+            Dias = Hora / 24;
+
+            Console.WriteLine("Dias: " + Dias);
+            Console.WriteLine("Minutos: " + Minutos);
+            Console.WriteLine("Segundos: "+ Segundos);
+        }
+    }
+}
