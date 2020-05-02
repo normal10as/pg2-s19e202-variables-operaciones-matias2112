3cd3127713715b66ec3604c39b4a0b7b64788094
Santiago Del Rosario <delro_chinchu@live.com.ar>
2020-05-01 23:34:47 -0300

    1.2

---
+++ VariablesOperacionesCS/CotizaciónTerreno/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CotizaciónTerreno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Ancho, Lado, CostoM2;
+            Console.Write("Ingrese el ancho: ");
+            Ancho =double.Parse(Console.ReadLine());
+            Console.Write("Ingrese la longitud: ");
+            Lado = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el costo por m2: ");
+            CostoM2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("El costo del terreno es de $: " + (Ancho * Lado) * CostoM2);
+        }
+    }
+}
