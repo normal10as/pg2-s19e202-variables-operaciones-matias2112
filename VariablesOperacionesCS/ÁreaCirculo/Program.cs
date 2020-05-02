c727c5c7066c72f6c5684b87b286cf8b2c86e345
Santiago Del Rosario <delro_chinchu@live.com.ar>
2020-05-02 12:11:09 -0300

    1.9

---
+++ VariablesOperacionesCS/ÁreaCirculo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ÁreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Radio, Area;
+            Console.Write("Ingrese el radio: ");
+            Radio =double.Parse( Console.ReadLine());
+            Area = Math.PI * (Math.Pow(Radio, 2));
+            Console.WriteLine("El área es: " + Area);
+        }
+    }
+}
