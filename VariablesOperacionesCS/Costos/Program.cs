3fad64ebaebf873e3af5278258200d8e3b9c9c9a
Santiago Del Rosario <delro_chinchu@live.com.ar>
2020-05-01 23:22:43 -0300

    1.1

---
+++ VariablesOperacionesCS/Costos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Costos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string DescArticulo;
+            
+            double Costo=0, UnidadesProducidas=0;
+            Console.Write("Ingrese la descripción: ");
+            DescArticulo=Console.ReadLine();
+
+            Console.Write("Ingrese las Unidades producidas: ");
+            UnidadesProducidas =double.Parse(Console.ReadLine());
+            
+            Costo = (UnidadesProducidas * 3.5) + 10700;
+
+            Console.WriteLine("Descripcion: " + DescArticulo);
+            Console.WriteLine("Unidades Producidas: " + UnidadesProducidas);
+            Console.WriteLine("Costo: " + Costo);
+
+            
+        }
+    }
+}
