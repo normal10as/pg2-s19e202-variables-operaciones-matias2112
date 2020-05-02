faa9955cdf0f6dae5d3e214fb4e8dc4dd0ec12fe
Santiago Del Rosario <delro_chinchu@live.com.ar>
2020-05-02 12:13:52 -0300

    1.10

---
+++ VariablesOperacionesCS/CotizaciónDólar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CotizaciónDólar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Dolar;
+            Console.Write("1 dolar = 65.93 pesos-----Ingrese la cantidad de Dólares: ");
+            Dolar = float.Parse(Console.ReadLine());
+            Console.WriteLine("Cantidad de pesos: $" + Dolar * 65.93);
+        }
+    }
+}
