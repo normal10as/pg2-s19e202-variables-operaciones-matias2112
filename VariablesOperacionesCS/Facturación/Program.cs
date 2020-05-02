0ec2c5c719d14ab4f2b59fff6880d292de738f45
Santiago Del Rosario <delro_chinchu@live.com.ar>
2020-05-02 12:08:44 -0300

    1.8

---
+++ VariablesOperacionesCS/Facturación/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Facturación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Articulo;
+            double Costo=0,Utilidad=0, Impuesto=0;
+            Console.Write("Ingrese el nombre: ");
+            Articulo = Console.ReadLine();
+            Console.Write("Ingrese el costo: ");
+            Costo = double.Parse(Console.ReadLine());
+            Utilidad = (Costo * 150) / 100;
+            Impuesto = (Costo * 21) / 100;
+            Console.WriteLine("Articulo: " + Articulo);
+            Console.WriteLine("Utilidad: $" + Utilidad);
+            Console.WriteLine("Impuesto: $" + Impuesto);
+           Console.WriteLine("Precio Venta: $" + (Utilidad + Impuesto));
+        }
+    }
+}
