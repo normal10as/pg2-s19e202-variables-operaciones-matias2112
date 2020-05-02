e2724726016096222f2e973354dfa29caf85e2b2
Santiago Del Rosario <delro_chinchu@live.com.ar>
2020-05-02 11:59:44 -0300

    1.5

---
+++ VariablesOperacionesCS/Cuadratica/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cuadratica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=1, b=5, c=2;
+            double Cuadratica;
+
+            Cuadratica = (Math.Pow(b, 2) - (4 * a * c)) / (2 * a);
+
+            Console.WriteLine("La cuadratica es: " + Cuadratica);
+        }
+    }
+}
